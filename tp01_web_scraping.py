e62ba971bde7d53a3c6315c72df90158eaae981f
Hadil-Guenoua <hadilmayar100@gmail.com>
2025-10-07 20:14:48 +0100

    Add files via upload

--- tp01_web_scraping.py
+++ tp01_web_scraping.py
@@ -2,12 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# 1️⃣ Fetch the content of the web page
-url = "https://quotes.toscrape.com"   # موقع تجريبي مخصص للتدريب على web scraping
+# Fetch the content of the web page
+url = "https://quotes.toscrape.com" 
 response = requests.get(url)
-print(response.text)  # عرض كود HTML للصفحة
+print(response.text)  
 
-# 2️⃣ Use BeautifulSoup to extract all quotes from the page
+# Use BeautifulSoup to extract all quotes from the page
 soup = BeautifulSoup(response.text, "html.parser")
 quotes = soup.find_all("span", class_="text")
 authors = soup.find_all("small", class_="author")
@@ -15,7 +15,7 @@ authors = soup.find_all("small", class_="author")
 quotes_list = [q.text for q in quotes]
 authors_list = [a.text for a in authors]
 
-# 3️⃣ Save results to a CSV file using pandas
+# Save results to a CSV file using pandas
 data = {"Quote": quotes_list, "Author": authors_list}
 df = pd.DataFrame(data)
 df.to_csv("quotes.csv", index=False)
